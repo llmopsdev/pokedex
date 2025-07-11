f2cceaf578ad69131897ab13bc81dd00b3e2f55d
llmopsdev <llmopsdev@gmail.com>
2025-07-11 11:17:34 -0700

    repl input

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,24 @@
+// repl.js actually refers to repl.ts
+import { createInterface } from "readline";
+import { startREPL } from "./repl.js";
+import { cleanInput } from "./repl.js";
+
+const readInput = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "Pokedex> ",
+});
+
+
 function main() {
-  console.log("Hello, world!");
+  startREPL();
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex> ",
+  });
+  rl.prompt();
+  rl.on("line", cleanInput);
 }
 
 main();
--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,24 @@
+import { createInterface } from "readline";
+
 export function cleanInput(input: string) : string[]{
   return input.toLowerCase().trim().split(" ").filter((word)=> word !== "");
 }
+export function startREPL(){
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex> ",
+  });
+  rl.prompt();
+  rl.on("line", async (line) => {
+    const inputArray = cleanInput(line);
+    if (inputArray.length === 0) {
+      rl.prompt();
+      return;
+    }
+    else {
+      console.log(`Your command was: ${inputArray[0]}`);
+      rl.prompt();
+    }
+  });
+};
